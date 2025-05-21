2261e6ff097675744f71e6a75be052dd9f246e5c
pitarchmiguel <pitarchmiguel@gmail.com>
2025-05-21 22:37:51 +0200

    funcionara

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  integrations: [
+    tailwind({
+      // Configuración de Tailwind
+      applyBaseStyles: false, // Desactivamos los estilos base de Tailwind ya que los tenemos en global.css
+    })
+  ]
 });
